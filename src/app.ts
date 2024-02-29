507310e575c8569282201b9a6e8f0fb6ad7293af
Saulo0097 <saulohermida1997@gmail.com>
2024-02-29 17:38:58 -0500

    connecting to mongo database

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import "dotenv/config"
 import express, { json } from "express"
 import cors from "cors";
+import { mongoDB } from "./config/mongo.config";
 
 const PORT = process.env.PORT || 3000
 const app = express()
@@ -9,5 +10,5 @@ app.use(cors())
 app.use(json())
 
 
-
+mongoDB().then(()=>{console.log('Connection to Database is Ready')})
 app.listen(PORT, ()=> {console.log(`🚀 Server Ready, Listening in Port ${PORT}`)})
---
+++ src/config/mongo.config.ts
@@ -0,0 +1,7 @@
+import "dotenv/config"
+import { connect } from "mongoose"
+
+export const mongoDB = async (): Promise<void> => {
+    const DB_URI = process.env.DB_URI|| 'mongodb://localhost:27017/api-rest-ts'
+    await connect(DB_URI)
+}
