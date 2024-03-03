d6068029a433c1767ca457a6f40101fab62ee58c
Saulo0097 <saulohermida1997@gmail.com>
2024-03-03 11:31:02 -0500

    creating getAll method in usercontroller

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,6 +1,14 @@
 import { Request, Response } from "express";
+import { getUsers } from "../services/user";
 
-export const getAll = async (req: Request, res: Response): Promise<void> => {}
+export const getAll = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const response = await getUsers()
+        res.sendStatus(200).send(response)
+    } catch (error:any) {
+        res.sendStatus(400).json(error.message??  error)
+    }
+}
 export const getOneById = async (req: Request, res: Response): Promise<void> => {}
 export const postOne = async (req: Request, res: Response): Promise<void> => {}
 export const updateOne = async (req: Request, res: Response): Promise<void> => {}
