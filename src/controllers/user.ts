bc281f04af3b118e9a79453ea27312c4680b1bb6
Saulo0097 <saulohermida1997@gmail.com>
2024-03-03 11:37:04 -0500

    creating getOneById method in usercontroller

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,15 +1,24 @@
 import { Request, Response } from "express";
-import { getUsers } from "../services/user";
+import { getUser, getUsers } from "../services/user";
 
-export const getAll = async (req: Request, res: Response): Promise<void> => {
+export const getAll = async (_req: Request, res: Response): Promise<void> => {
     try {
         const response = await getUsers()
         res.sendStatus(200).send(response)
     } catch (error:any) {
-        res.sendStatus(400).json(error.message??  error)
+        res.sendStatus(400).json({data:error?.message ||  error})
     }
 }
-export const getOneById = async (req: Request, res: Response): Promise<void> => {}
+export const getOneById = async (req: Request, res: Response): Promise<void> => {
+    try{
+        const id = +req.params.id
+        if(!id) res.sendStatus(404).json({data: "ID_PARAMS_NOT_FOUND"})
+        const response = await getUser(id)
+        res.sendStatus(200).send(response)       
+    }catch(error: any){
+        res.sendStatus(404).json({data: error?.message || error})
+    }
+}
 export const postOne = async (req: Request, res: Response): Promise<void> => {}
 export const updateOne = async (req: Request, res: Response): Promise<void> => {}
 export const deleteOne = async (req: Request, res: Response): Promise<void> => {}
