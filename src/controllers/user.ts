e84d8546452f39eba841e5ccca9b42ecdb5806fd
Saulo0097 <saulohermida1997@gmail.com>
2024-03-03 10:39:00 -0500

    creating user controller

---
+++ src/controllers/user.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from "express";
+
+export const getAll = async (req: Request, res: Response): Promise<void> => {}
+export const getOneById = async (req: Request, res: Response): Promise<void> => {}
+export const postOne = async (req: Request, res: Response): Promise<void> => {}
+export const updateOne = async (req: Request, res: Response): Promise<void> => {}
+export const deleteOne = async (req: Request, res: Response): Promise<void> => {}
