42fb5b4a6aca6ee3821b44241e92fe4cb5cc9b9f
Saulo0097 <saulohermida1997@gmail.com>
2024-03-03 22:29:28 -0500

    updating regex for validation datetime types in booking controlers

--- src/validators/createBookingValidator.ts
+++ src/validators/createBookingValidator.ts
@@ -7,12 +7,12 @@ export const createBookingValidator = [
         .exists()
         .trim()
         .notEmpty()
-        .isDate(),
+        .matches('^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) (2[0-3]|[01][0-9]):[0-5][0-9]$'),
     check('dateEnd')
         .exists()
         .trim()
         .notEmpty()
-        .isDate(),
+        .matches('^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) (2[0-3]|[01][0-9]):[0-5][0-9]$'),
     check('userId')
         .exists()
         .trim()
