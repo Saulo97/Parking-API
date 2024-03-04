1d73157578da3527f0d9fe310f4939d2274589d8
Saulo0097 <saulohermida1997@gmail.com>
2024-03-03 21:19:26 -0500

    add bookings routes to main router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { userRoutes } from "./user";
 import { parkingRouter } from "./parking";
+import { bookingRouter } from "./booking";
 
 export const router = Router()
 
 router.use(userRoutes)
 router.use(parkingRouter)
+router.use(bookingRouter)
