94aeaf5aa820317fdcae64028cdf776c9aa316f6
Saulo0097 <saulohermida1997@gmail.com>
2024-03-03 22:32:12 -0500

    change regex validation dateStart

--- src/validators/updateBookingValidator.ts
+++ src/validators/updateBookingValidator.ts
@@ -7,12 +7,12 @@ export const updateBookingValidator = [
         .optional() 
         .trim()
         .notEmpty()
-        .isDate(),
+        .matches('^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) (2[0-3]|[01][0-9]):[0-5][0-9]$'),
     check('dateEnd')
         .optional()
         .trim()
         .notEmpty()
-        .isDate(),
+        .matches('^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) (2[0-3]|[01][0-9]):[0-5][0-9]$'),
     check('userId')
         .optional()
         .trim()
