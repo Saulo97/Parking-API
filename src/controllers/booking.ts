8d5672da9a3ad0dc95a683b8b2ff594a93c9a407
Saulo0097 <saulohermida1997@gmail.com>
2024-03-03 21:08:02 -0500

    creating methods in booking controllers

---
+++ src/controllers/booking.ts
@@ -0,0 +1,65 @@
+import { Request, Response } from "express";
+import { handleError } from "../utils/error.handle";
+import { createBooking, deleteBooking, getBooking, getBookings, updateBooking } from "../services/booking";
+import { BookingInput } from "../interfaces/booking.interface";
+
+export const getAll = async (_req: Request, res: Response): Promise<void> => {
+    try {
+        const response = await getBookings()
+        res.status(200).json(response)
+    } catch (error:any) {
+        handleError(res, error.status, error.message)
+    }
+}
+export const getOneById = async (req: Request, res: Response): Promise<void> => {
+    try{
+        const id = +req.params.id
+        if(!id || typeof id !== "number" ) throw {status: 400, message: "Error In Param Id "}
+        const response = await getBooking(id)
+        res.status(200).json(response)       
+    }catch(error:any) {
+        handleError(res, error.status, error.message)
+    }
+}
+export const postOne = async (req: Request, res: Response): Promise<void> => {
+    try{
+        const {dateStart, dateEnd, userId, placeId} = req.body
+        const newBooking: BookingInput = {
+            dateStart: dateStart,
+            dateEnd: dateEnd,
+            userId: userId,
+            placeId: placeId
+        }
+        const response = await createBooking(newBooking)
+        res.status(201).json(response)
+    }catch(error:any) {
+        handleError(res, error.status, error.message)
+    }
+}
+export const updateOne = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const id = +req.params.id
+        if(!id || typeof id !== "number" ) throw {status: 400, message: "Error In Param Id "}
+        const {dateStart, dateEnd, userId, placeId} = req.body
+        const newBooking: BookingInput = {
+            dateStart: dateStart,
+            dateEnd: dateEnd,
+            userId: userId,
+            placeId: placeId
+        }
+        const response = await updateBooking(id,newBooking)
+        res.status(201).json(response)
+    } catch (error:any) {
+        handleError(res, error.status, error.message)
+    }
+}
+export const deleteOne = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const id = +req.params.id
+        if(!id || typeof id !== "number" ) throw {status: 400, message: "Error In Param Id "}
+        await deleteBooking(id)
+        res.status(200).json({data:"BOOKING_DELETED_SUCCESSFULY"})
+    } catch (error:any) {
+        handleError(res, error.status, error.message)
+    }
+}
