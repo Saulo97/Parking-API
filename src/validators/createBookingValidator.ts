fb69d7083ee6c42b401aa649b50df152b984f628
Saulo0097 <saulohermida1997@gmail.com>
2024-03-08 23:02:22 -0500

    add validations for dateStart and endStart

--- src/validators/createBookingValidator.ts
+++ src/validators/createBookingValidator.ts
@@ -3,16 +3,35 @@ import { NextFunction, Request, Response } from "express";
 import { validateResult } from "../utils/validateResult";
 
 export const createBookingValidator = [
+    
     check('dateStart')
         .exists()
         .trim()
         .notEmpty()
-        .matches('^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])T(2[0-3]|[01][0-9]):[0-5][0-9]$'),
+        .matches('^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])T(2[0-3]|[01][0-9]):[0-5][0-9]$')
+        .custom((value, {req})=>{
+            const now = new Date()
+            if(value>req.body.dateEnd){
+                throw new Error("The start date is after that end start")
+            }else if(value< now.toISOString()){
+                throw new Error("The start date is before today")
+            }else{
+                return true
+            }
+        }),
     check('dateEnd')
         .exists()
         .trim()
         .notEmpty()
-        .matches('^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])T(2[0-3]|[01][0-9]):[0-5][0-9]$'),
+        .matches('^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])T(2[0-3]|[01][0-9]):[0-5][0-9]$')
+        .custom((value, {req})=>{
+            const now = new Date()
+            if(value< now.toISOString()){
+                throw new Error("The end date is before today")
+            }else{
+                return true
+            }
+        }),
     check('userId')
         .exists()
         .trim()
--- src/validators/updateBookingValidator.ts
+++ src/validators/updateBookingValidator.ts
@@ -7,12 +7,30 @@ export const updateBookingValidator = [
         .optional() 
         .trim()
         .notEmpty()
-        .matches('^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) (2[0-3]|[01][0-9]):[0-5][0-9]$'),
+        .matches('^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])T(2[0-3]|[01][0-9]):[0-5][0-9]$')
+        .custom((value, {req})=>{
+            const now = new Date()
+            if(value>req.body.dateEnd){
+                throw new Error("The start date is after that end start")
+            }else if(value< now.toISOString()){
+                throw new Error("The start date is before today")
+            }else{
+                return true
+            }
+        }),
     check('dateEnd')
         .optional()
         .trim()
         .notEmpty()
-        .matches('^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) (2[0-3]|[01][0-9]):[0-5][0-9]$'),
+        .matches('^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])T(2[0-3]|[01][0-9]):[0-5][0-9]$')
+        .custom((value, {req})=>{
+            const now = new Date()
+            if(value< now.toISOString()){
+                throw new Error("The end date is before today")
+            }else{
+                return true
+            }
+        }),
     check('userId')
         .optional()
         .trim()
