17878502e50f70ab54ed1b822b4012e3cb1bcbdf
Saulo0097 <saulohermida1997@gmail.com>
2024-03-09 23:08:42 -0500

    creating admin auth middelware

---
+++ src/middlewares/adminAuthorization.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from "express";
+import { ErrorResponse } from "../interfaces/errorResponse.interface";
+import { Roles } from "../interfaces/user.interface";
+
+export const adminAuthorization = async ( req: Request, res: Response, next: NextFunction)=>{
+    try {
+        const {rol} = req.body.user
+        if(rol !== Roles.admin){
+            const errorResponse : ErrorResponse = {status: 401, message: "This User has not permission"}
+            return res.status(401).json(errorResponse)  
+        }
+        next()
+    } catch (error) {
+        const errorResponse : ErrorResponse = {status: 401, message: "This User has not permission"}
+        return res.status(401).json(errorResponse)
+    }
+}
