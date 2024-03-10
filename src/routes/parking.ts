6b32e64100a69d413d580ed0bb9e4def1cbb16f3
Saulo0097 <saulohermida1997@gmail.com>
2024-03-09 23:25:15 -0500

    add autherization for bookings routes

--- src/routes/parking.ts
+++ src/routes/parking.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { deleteOne, getAll, getOneById, postOne, updateOne } from "../controllers/parking";
 import { parkingValidator } from "../validators/parkingValidator";
+import { authorize } from "../middlewares/authorize";
+import { adminAuthorization } from "../middlewares/adminAuthorization";
 
 export const parkingRouter = Router()
 
 parkingRouter
         .get("/parkings", getAll)
-        .post("/parkings", parkingValidator,postOne)
-        .put("/parkings/:id", parkingValidator,updateOne)
-        .delete("/parkings/:id", deleteOne)
+        .post("/parkings", authorize, adminAuthorization,parkingValidator,postOne)
+        .put("/parkings/:id", authorize, adminAuthorization,parkingValidator,updateOne)
+        .delete("/parkings/:id", authorize, adminAuthorization,deleteOne)
         .get("/parkings/:id", getOneById)
