3100ba86d084400867b96b99ce9b50915e3b9fda
Saulo0097 <saulohermida1997@gmail.com>
2024-03-14 19:21:10 -0400

    creating log documentation

--- src/routes/log.ts
+++ src/routes/log.ts
@@ -7,3 +7,34 @@ export const logRoutes = Router()
 
 
 logRoutes.get('/logs', authorize, adminAuthorization, getAll)
+/**
+         * @swagger
+         * /logs:
+         *   get:
+         *      tags: 
+         *              - Logs
+         *      summary: Obtener los logs del api
+         *      description: Devuelve un arreglo de Logs. Solamente el admin tiene acceso a este endpoint
+         *      security: 
+         *              - bearerAuth: []
+         *      responses:
+         *              200:
+         *                      description: Succeful login
+         *                      content:
+         *                              application/json:
+         *                                      schema:
+         *                                              $ref: '#/components/schemas/ApiResponse'
+         *              401:
+         *                      description: Error by User has Not authorization
+         *                      content:
+         *                              application/json:
+         *                                      schema:
+         *                                              $ref: '#/components/schemas/ApiErrorAuthResponse'
+         *              500:
+         *                      description: Internal Server Error
+         *                      content:
+         *                              application/json:
+         *                                      schema:
+         *                                              $ref: '#/components/schemas/ApiErrorResponse'
+         *              
+         */
